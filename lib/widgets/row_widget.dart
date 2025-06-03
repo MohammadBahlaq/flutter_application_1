68127fba50f6ff84697d282e1a12977ced869e7c
Mohammad Bahlaq <Bahlaq57@gmail.com>
2025-06-03 14:08:25 -0700

    first commit

---
+++ lib/widgets/row_widget.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class RowWidget extends StatelessWidget {
+  const RowWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Row Widget")),
+      body: Row(
+        //Row put the widgets horizantal
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Container(height: 50, width: 50, color: Colors.red),
+          Container(height: 50, width: 50, color: Colors.blue),
+          Container(height: 50, width: 50, color: Colors.green),
+          Container(height: 50, width: 50, color: Colors.black),
+        ],
+      ),
+    );
+  }
+}
