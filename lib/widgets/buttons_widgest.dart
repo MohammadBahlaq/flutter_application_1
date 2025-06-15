f636904a0e9c72b8d166abbfb0de20d4494d58e3
Mohammad Bahlaq <Bahlaq57@gmail.com>
2025-06-14 19:07:49 -0700

    nothing important

--- lib/widgets/buttons_widgest.dart
+++ lib/widgets/buttons_widgest.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class FloatingButton extends StatelessWidget {
-  const FloatingButton({super.key});
+class ButtonsWidgets extends StatelessWidget {
+  const ButtonsWidgets({super.key});
 
   @override
   Widget build(BuildContext context) {
