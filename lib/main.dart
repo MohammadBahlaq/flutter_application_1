845c10e2b43259b0b0120ef9c91b96d4c617702c
Mohammad Bahlaq <Bahlaq57@gmail.com>
2025-06-15 14:24:59 -0700

    textButton, listview, pageview, gridview, customWidget

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,20 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/widgets/app_bar_widget.dart';
+import 'package:flutter_application_1/widgets/buttons_widgest.dart';
 import 'package:flutter_application_1/widgets/column_widget.dart';
 import 'package:flutter_application_1/widgets/container_widget.dart';
-import 'package:flutter_application_1/widgets/floating_button.dart';
+import 'package:flutter_application_1/widgets/custom_buttons.dart';
 import 'package:flutter_application_1/widgets/gesture_inkwell_buttons.dart';
+import 'package:flutter_application_1/widgets/grid_view_widget.dart';
 import 'package:flutter_application_1/widgets/image_widget.dart';
+import 'package:flutter_application_1/widgets/list_view_widget.dart';
 import 'package:flutter_application_1/widgets/padding_widget.dart';
+import 'package:flutter_application_1/widgets/page_view_widget.dart';
 import 'package:flutter_application_1/widgets/row_widget.dart';
+import 'package:flutter_application_1/widgets/single_child_scroll_view_widget.dart';
 import 'package:flutter_application_1/widgets/stack_widget.dart';
+import 'package:flutter_application_1/widgets/text_button_widget.dart';
 
 void main() {
   runApp(const MyApp());
@@ -26,7 +32,12 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: PaddingWidget(),
+      home: ListViewWidget(),
+      // routes: {
+      //   'pageOne': (_) => PageOne(),
+      //   'pageTwo': (_) => PageTwo(),
+      //   'pagethree': (_) => PageThree(),
+      // },
     );
   }
 }
